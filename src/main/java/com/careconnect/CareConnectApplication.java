9f51176c91a30558d90dfb00b15e2c8a657ea5dc
Shweta Chandankhede <shwetachandankhede546@gmail.com>
2026-01-21 23:21:35 -0800

    added env for local and production
    added env for local and production

--- src/main/java/com/careconnect/CareConnectApplication.java
+++ src/main/java/com/careconnect/CareConnectApplication.java
@@ -1,5 +1,6 @@
 package com.careconnect;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CareConnectApplication {
 
     public static void main(String[] args) {
+
+        // Load .env BEFORE Spring starts
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            if (System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+
         SpringApplication.run(CareConnectApplication.class, args);
     }
 }
